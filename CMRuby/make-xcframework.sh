c66d7752828bfe82be342259df707216748e0181
rinsuki <428rinsuki+git@gmail.com>
2020-09-06 16:52:58 +0900

    adopt to cinderella-project/mruby changes

--- CMRuby/make-xcframework.sh
+++ CMRuby/make-xcframework.sh
@@ -2,12 +2,12 @@
 set -xe
 
 rm -rf frameworks
-for target in host ios ios_sim
+for target in host iphoneos iphonesimulator
 do
     mkdir -p frameworks/$target/CMRuby.framework/{Headers,Modules}
     cp -r submodules/mruby/include/ frameworks/$target/CMRuby.framework/Headers
     cp module.modulemap frameworks/$target/CMRuby.framework/Modules
-    cp submodules/mruby/build/$target/lib/libmruby.a frameworks/$target/CMRuby.framework/CMRuby
+    cp submodules/mruby/build/$target/bin/mruby.dylib frameworks/$target/CMRuby.framework/CMRuby
     pushd frameworks/$target/CMRuby.framework/Headers
     find ./mruby -type f | xargs sed -Ei '' 's/# *include [<"]mruby\/(.+)[">]/#include "\1"/g'
     find ./mruby -type f | xargs sed -Ei '' 's/# *include <(mruby\.h)>/#include "..\/\1"/g'
@@ -21,7 +21,7 @@ done
 rm -rf ./CMRuby.xcframework
 xcodebuild -create-xcframework \
     -framework frameworks/host/CMRuby.framework \
-    -framework frameworks/ios/CMRuby.framework \
-    -framework frameworks/ios_sim/CMRuby.framework \
+    -framework frameworks/iphoneos/CMRuby.framework \
+    -framework frameworks/iphonesimulator/CMRuby.framework \
     -output ./CMRuby.xcframework
 cp submodules/mruby/build/host/LEGAL ./CMRuby.xcframework/
